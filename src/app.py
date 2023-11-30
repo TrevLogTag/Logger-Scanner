0f6bd9d6ccf0c62a508e61cc0799ff022110e25a
TrevLogTag <LogTagEngInternE@gmail.com>
2023-11-30 15:37:47 +1300

    Added files

---
+++ src/app.py
@@ -0,0 +1,149 @@
+import asyncio
+from bleak import *
+from bleak.backends.scanner import AdvertisementData
+from aioconsole import ainput
+import matplotlib.pyplot as pltpip
+from datetime import datetime
+
+devices = ["FE:A9:04:78:B6:C9", "CA:84:BC:CC:11:21"]
+TEMPERATURE_UUID = "00002a1c-0000-1000-8000-00805f9b34fb"
+HUMIDITY_UUID = "00002a6f-0000-1000-8000-00805f9b34fb"
+BATTERY_UUID = "00002a19-0000-1000-8000-00805f9b34fb"
+UART_RX_UUID = (
+    "6e400003-b5a3-f393-e0a9-e50e24dcca9e"  # Nordic NUS characteristic for RX
+)
+UART_TX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+
+class Connection:
+    isConnect = False
+    chosen_index = None
+
+    def __init__(self, loop: asyncio.AbstractEventLoop, read_characteristic: str):
+        self.read_characteristic = read_characteristic
+        self.last_packet_time = datetime.now()
+        self.connected = False
+        self.loop = loop
+
+        self.rx_data = []
+        self.rx_timestamps = []
+        self.rx_delays = []
+        self.temp_data = []
+
+    async def manager(self):
+        print("STARTING CONNECTION MANAGER")
+        while True:
+            if self.isConnect:
+                await self.connect()
+            else:
+                await self.select_device()
+
+    async def connect(self):
+        isConnectFlag = False
+        while isConnectFlag == False:
+            print("Connecting to " + devices[int(self.chosen_index)])
+            client = BleakClient(devices[int(self.chosen_index)])
+            try:
+                await client.connect()
+                print("Connection Successful")
+
+                while 1:
+                    await notification_manager(client)
+                    # svcs = await client.get_services()
+                    # for service in svcs:
+                    #    print(service)
+                isConnect = True
+            except Exception as e:
+                print(e)
+            finally:
+                await client.disconnect()
+            await asyncio.sleep(2.0)
+
+    async def select_device(self):
+        print("Select device to connect")
+        print("0: Name: LOG-IC 360 | Address: FE:A9:04:78:B6:C9")
+        print("1: Name: TZ-BT04 | Address: CA:84:BC:CC:11:21")
+
+        # Get input
+        while 1:
+            response = await ainput()
+            if response == "0":
+                self.isConnect = True
+                self.chosen_index = response
+                break
+            elif response == "1":
+                self.isConnect = True
+                self.chosen_index = response
+                break
+            else:
+                print("Invalid Input")
+
+    def record_time_info(self):
+        present_time = datetime.now()
+        self.rx_timestamps.append(present_time)
+        self.rx_delays.append((present_time - self.last_packet_time).microseconds)
+        self.last_packet_time = present_time
+
+    def clear_lists(self):
+        self.rx_data.clear()
+        self.rx_delays.clear()
+        self.rx_timestamps.clear()
+
+
+async def main():
+    isConnect = False
+    while isConnect == False:
+        print("Connecting to " + devices[0])
+        client = BleakClient(devices[0])
+        try:
+            await client.connect()
+            BleakScanner.advertisement_data
+            while 1:
+                await notification_manager(client)
+
+            isConnect = True
+        except Exception as e:
+            print(e)
+        finally:
+            await client.disconnect()
+        await asyncio.sleep(2.0)
+
+
+async def notification_manager(client):
+    print("Waiting...")
+    await client.start_notify(TEMPERATURE_UUID, temp_notification_handler)
+    await client.start_notify(HUMIDITY_UUID, humidity_notification_handler)
+    await asyncio.sleep(2.0)
+    await client.stop_notify(TEMPERATURE_UUID)
+    await client.stop_notify(HUMIDITY_UUID)
+
+
+def notification_handler(sender, data):
+    print("{0}: {1}".format(sender, data))
+
+
+def temp_notification_handler(sender, data):
+    current_temperature = int.from_bytes(data, byteorder="little", signed=True) / 100
+    print("Temperature: {0} Celsius".format(current_temperature))
+
+
+def humidity_notification_handler(sender, data):
+    current_humidity = int.from_bytes(data, byteorder="little", signed=False) / 100
+    print("Humidity: {0} %".format(current_humidity))
+
+
+if __name__ == "__main__":
+    # Create the event loop.
+    loop = asyncio.get_event_loop()
+
+    connection = Connection(loop, UART_RX_UUID)
+    try:
+        asyncio.ensure_future(connection.manager())
+        loop.run_forever()
+        # asyncio.run(main())
+    except KeyboardInterrupt:
+        # CTRL + C
+        print()
+        print("User stopped program")
+    finally:
+        print("Disconnecting")
---
+++ src/tzone_reader.py
@@ -0,0 +1,177 @@
+import asyncio
+from bleak import *
+from datetime import timedelta
+import datetime
+from aioconsole import ainput
+
+LOGIC_360 = "FE:A9:04:78:B6:C9"
+TZ_BT04 = "CA:84:BC:CC:11:21"
+
+password_UUID = "27763B13-999C-4D6A-9FC4-c7272BE10900"
+data_mode_UUID = "27763B31-999C-4D6A-9FC4-C7272BE10900"
+sync_dataswitch_UUID = "27763B21-999C-4D6A-9FC4-C7272BE10900"  # To read past data
+degree_sign = "\N{DEGREE SIGN}"
+
+
+async def main():
+    print("TZONE READER")
+    print("0: Broadcasted Data")
+    print("1: Stored Data")
+    print("2: Output services")
+
+    # Get input
+    while 1:
+        response = await ainput()
+        if response == "0":
+            await get_broadcast_data()
+            break
+        elif response == "1":
+            await get_stored_data()
+            break
+        elif response == "2":
+            await print_services()
+            break
+        else:
+            print("Invalid Input")
+
+
+# Get stored data function
+async def get_stored_data():
+    print("STORED DATA =>")
+    isConnect = False
+    while isConnect == False:
+        print("Connecting to " + TZ_BT04)
+        client = BleakClient(TZ_BT04, timeout=15.0)
+        try:
+            await client.connect()
+            print("Connection Successful")
+            await get_data(client)
+
+            isConnect = True
+        except Exception as e:
+            print(e)
+        finally:
+            await client.disconnect()
+
+
+# Get broadcasted data function
+async def get_broadcast_data():
+    print("BROADCASTED DATA =>")
+    scanner = BleakScanner(detection_callback)
+    print("Starting scanner...")
+    while True:
+        await scanner.start()
+        await asyncio.sleep(30.0)
+        await scanner.stop()
+        print("(Re)starting scanner")
+
+
+# Print services
+async def print_services():
+    isConnect = False
+    while isConnect == False:
+        print("Connecting to " + TZ_BT04)
+        client = BleakClient(TZ_BT04, timeout=15.0)
+        try:
+            await client.connect()
+            print("Connection Successful")
+            services = await client.get_services()
+            for svcs in services:
+                print(svcs)
+
+            isConnect = True
+        except Exception as e:
+            print(e)
+        finally:
+            await client.disconnect()
+
+
+# Handles advertising packets scanned
+def detection_callback(device: BLEDevice, advertisement_data: AdvertisementData):
+    if device.address == TZ_BT04:
+        dict = advertisement_data.service_data
+        data = dict["0000cbff-0000-1000-8000-00805f9b34fb"].hex()
+        device_id = data[8:16]
+        battery = int(data[16:18], 16)
+        temp = int(data[20:24], 16)
+        parsed_temp = round((float(temp) * 0.01), 1)
+        humidity = int(data[24:28], 16)
+        parsed_humidity = round((float(humidity) * 0.01), 1)
+        print(
+            "DEVICE ID: "
+            + device_id
+            + " | BATTERY: "
+            + str(battery)
+            + "% | TEMP: "
+            + str(parsed_temp)
+            + degree_sign
+            + "C | HUMIDITY: "
+            + str(parsed_humidity)
+            + "%"
+        )
+
+
+# This function is called everytime a packet is sent
+def notification_handler(sender, data):
+    # print("{0}: {1}".format(sender, data.hex()))
+    # print(data.hex())
+    byte_parser(data)
+
+
+def byte_parser(data: bytearray):
+    data_hex = bytes(data).hex()
+    first_reading = data_hex[0:14]
+    second_reading = data_hex[14:28]
+
+    # Parse readings
+    reader(first_reading)
+    reader(second_reading)
+
+
+def reader(reading: str):
+    date = reading[0:8]
+    values = reading[8:14]
+
+    # Convert to datetime format
+    dtime_obj = datetime.datetime.utcfromtimestamp(int(date, 16))
+    dtime_obj += timedelta(hours=12)
+    datetime_str = dtime_obj.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Get temperature and humidity
+    if values:
+        # Calculate temp
+        temp_binary = (int(values, 16) >> 6) & 0x007FF
+        temp = int(temp_binary)
+        temp = float(temp) * 0.1
+        temp = round(temp, 1)
+
+        # Calculate humidity
+        humidity_hex = values[0:2]
+        humidity_binary = (int(humidity_hex, 16)) >> 1
+        humidity = int(humidity_binary)
+
+        print(
+            datetime_str
+            + " | TEMP: "
+            + str(temp)
+            + degree_sign
+            + "C | HUMIDITY: "
+            + str(humidity)
+            + "%"
+        )
+
+
+async def get_data(client):
+    print("Send password...")
+    bytes_to_send = bytearray([0x00, 00, 00, 00, 00, 00])
+    print("SENT: 0x00, 00, 00, 00, 00, 00")
+    await client.write_gatt_char(password_UUID, bytes_to_send)
+
+    print()
+    print("Getting data...")
+    await client.start_notify(sync_dataswitch_UUID, notification_handler)
+    await asyncio.sleep(10.0)
+    await client.stop_notify(sync_dataswitch_UUID)
+
+
+asyncio.run(main())
